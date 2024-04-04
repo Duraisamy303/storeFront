eea469fc6bf5a325216a0ea9cccdb28228a3e436
Alaguraj1 <alaguraj259@gmail.com>
2024-04-04 14:31:37 +0530

    changes

--- src/components/breadcrumb/contact-breadcrumb.jsx
+++ src/components/breadcrumb/contact-breadcrumb.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import banner from "@assets/img/shop-banner.jpg";
 
-
 const ContactBreadcrumb = () => {
-  console.log("banner", banner)
+  console.log("banner", banner);
   return (
-    <section className="breadcrumb__area include-bg text-center pt-95 pb-50" style={{ backgroundImage: `url(${banner?.src})` }}>
+    <section
+      className="breadcrumb__area include-bg text-center pt-95 pb-50"
+      style={{ backgroundImage: `url(${banner?.src})` }}
+    >
       <div className="container">
         <div className="row">
           <div className="col-xxl-12">
-            <div className="breadcrumb__content p-relative z-index-1" style={{color:"white"}}>
-              <h3 className="breadcrumb__title" style={{color:"white", fontSize:"70px"}}>Keep In Touch with Us</h3>
-              <div className="breadcrumb__list">
+            <div
+              className="breadcrumb__content p-relative z-index-1"
+              style={{ color: "white" }}
+            >
+              <h3
+                className="breadcrumb__title"
+                style={{ color: "white", fontSize: "70px" }}
+              >
+                Keep In Touch with Us
+              </h3>
+              <div>
                 <span>
                   <a href="#">Home</a>
-                </span>
-                <span>Contact</span>
+                </span>{" "}
+                / <span>Contact</span>
               </div>
             </div>
           </div>
