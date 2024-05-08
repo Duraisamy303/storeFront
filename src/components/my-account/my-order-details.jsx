79b051db9036ed49ee4000b51c19683ffe6afc8e
Alaguraj1 <alaguraj259@gmail.com>
2024-05-08 09:53:32 +0530

    changes

--- src/components/my-account/my-order-details.jsx
+++ src/components/my-account/my-order-details.jsx
@@ -10,18 +10,20 @@ const MyOrderDetails = ({ data }) => {
   return (
     <section className="tp-checkout-area pb-50 pt-50">
       <div className="container">
-        <p style={{ color: "black" }}>
-          Order #{Data?.number} was placed on {FormatDate} and is currently{" "}
-          {Data?.statusDisplay}.
+        <p style={{ color: "gray" }}>
+          Order #<span style={{ color: "black" }}>{Data?.number}</span> was
+          placed on <span style={{ color: "black" }}>{FormatDate}</span> and is
+          currently{" "}
+          <span style={{ color: "black" }}>{Data?.statusDisplay}</span>.
         </p>
-
-        <h3>ORDER UPDATES</h3>
-        <p style={{ color: "black", marginBottom: "5px" }}>
-          Monday 22nd of April 2024, 03:38pm
-        </p>
-        <p style={{ color: "black" }}>hi mam</p>
-
-        <p>ORDER DETAILS</p>
+        <div className="pb-20 pt-20">
+          <h3 style={{ fontWeight: "300" }}>ORDER UPDATES</h3>
+          <p style={{ color: "gray", marginBottom: "5px" }}>
+            Monday 22nd of April 2024, 03:38pm
+          </p>
+          <p style={{ color: "gray" }}>hi mam</p>
+        </div>
+        <h3 style={{ fontWeight: "300" }}>ORDER DETAILS</h3>
         <div className="responsive-table">
           <table className="table">
             <thead>
@@ -33,7 +35,9 @@ const MyOrderDetails = ({ data }) => {
             <tbody>
               {Data?.lines.map((item, i) => (
                 <tr key={i}>
-                  <td scope="row">{item.productName} ({item?.quantity})</td>
+                  <td scope="row">
+                    {item.productName} ({item?.quantity})
+                  </td>
 
                   <td>{`${item?.totalPrice?.gross?.amount} `}</td>
                 </tr>
@@ -45,30 +49,53 @@ const MyOrderDetails = ({ data }) => {
             </tbody>
           </table>
         </div>
-<div className="row pt-50">
-<div className="col-md-6">
-<h4>BILLING ADDRESS</h4>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.firstName} {Data?.billingAddress?.lastName}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.streetAddress1}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.city}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.country?.country}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.postalCode}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.billingAddress?.phone}</p>
+        <div className="row pt-50">
+          <div className="col-md-6">
+            <h4  style={{ fontWeight: "300" }}>BILLING ADDRESS</h4>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.firstName} {Data?.billingAddress?.lastName}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.streetAddress1}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.city}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.country?.country}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.postalCode}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.billingAddress?.phone}
+            </p>
 
-
-<p></p>
-</div>
-<div className="col-md-6">
-<h4>SHIPPING ADDRESS</h4>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.firstName} {Data?.shippingAddress?.lastName}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.streetAddress1}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.city}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.country?.country}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.postalCode}</p>
-<p style={{color:"black", marginBottom:"2px"}}>{Data?.shippingAddress?.phone}</p>
-</div>
-</div>
-
+            <p></p>
+          </div>
+          <div className="col-md-6">
+            <h4  style={{ fontWeight: "300" }}>SHIPPING ADDRESS</h4>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.firstName}{" "}
+              {Data?.shippingAddress?.lastName}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.streetAddress1}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.city}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.country?.country}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.postalCode}
+            </p>
+            <p style={{ color: "gray", marginBottom: "0px" }}>
+              {Data?.shippingAddress?.phone}
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
