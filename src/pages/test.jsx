7825b6b81c19c59dc75a5d3759ab290d6a9945a0
Duraisamy <duraisamy.p@sanorac.com>
2024-08-31 17:49:58 +0530

    change

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,26 +1,7 @@
+import React from 'react'
 
-import { useGetCartAllListQuery, useGetCartListQuery } from "@/redux/features/card/cardApi";
-import { cart_list } from "@/redux/features/cartSlice";
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-
-export default function Test() {
-  const dispatch = useDispatch();
-
-  const { data: cartList, refetch: cartRefetch } = useGetCartListQuery();
-  console.log("cartList: ", cartList);
-
-  const { data: AllListChannel, refetch: AllListChannelREfresh } =
-    useGetCartAllListQuery({});
-
-  console.log("AllListChannel: ", AllListChannel);
-
-  useEffect(() => {
-    localStorage.removeItem("checkoutTokenUSD");
-    localStorage.removeItem("checkoutTokenINR");
-    localStorage.removeItem("checkoutToken");
-
-    dispatch(cart_list([]));
-  }, []);
-  return <div>test</div>;
+export default function test() {
+  return (
+    <div>test</div>
+  )
 }
